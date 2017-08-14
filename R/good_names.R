0c7c817a2f3ecd284f2a5232ef48ec48ae235794
Aidan Morrison <aidandmorrison@gmail,com>
2017-08-15 08:24:23 +1000

    first fucntion and setup stuff

---
+++ R/good_names.R
@@ -0,0 +1,29 @@
+#' @export
+#'
+good_names <- function(sheet) {
+  old_names <- colnames(sheet)%>%
+    data.frame()
+
+  working_names <- old_names$. %>%
+    str_replace_all(" ", "-")%>%
+    data.frame()
+  working_names$. <- working_names$. %>%
+    str_replace_all("\\(", "-") %>%
+    str_replace_all("\\)", "-") %>%
+    str_replace_all("-;", "-") %>%
+    str_replace_all("--", "-") %>%
+    str_replace_all("--", "-") %>%
+    str_replace_all("--", "-")
+
+  working_names$. <- working_names$. %>%
+    str_split("-")%>%
+    map( ~ .x %>% str_trunc(3, ellipsis = "") %>% .[. != ""] %>% str_c(collapse = "."))
+
+  good_names <- c("Date", c(working_names[-c(1),]%>%unlist()))%>%
+    as.vector()
+
+  colnames(sheet) <- good_names
+
+  sheet
+
+}
