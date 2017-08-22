ac28f9095418e50191157ab4b8599129ea40ccd8
Aidan Morrison <aidandmorrison@gmail,com>
2017-08-22 15:15:02 +1000

    to convert units to full nominal numbers

--- Scripts/Initial.R
+++ Scripts/Initial.R
@@ -1,13 +1,8 @@
-sheet <- readxl::read_xls("/Users/aidanmorrison/Dropbox/TrendLock/data/immigration/310101.xls", sheet = 2)
+sheet <- readxl::read_xls("/Users/aidanmorrison/Dropbox (Personal)/TrendLock/data/immigration/310101.xls", sheet = 2)
 
 sheet <- good_names(sheet = sheet)
 meta <- stash_meta(sheet)
 sheet <- cut_meta(sheet)
 sheet <- good_date(sheet)
-
-
-
-
-sheet %>%
-  to_numeric() %>%
-  str()
+sheet <- to_numeric(sheet)
+sheet <- nom_units(sheet, meta)
