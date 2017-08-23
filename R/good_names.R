bf41ab1037873227c22d9ad6f633e77a84991096
rosseji <rosseireland@gmail.com>
2017-08-23 10:51:38 +1000

    fixed nothing, but it works

--- R/good_names.R
+++ R/good_names.R
@@ -1,6 +1,6 @@
 #' @export
 #'
-good_names <- function(sheet, meta = FALSE) {
+good_names <- function(sheet = sheet, meta = FALSE) {
   checker <- meta
   old_names <- colnames(sheet)%>%
     data.frame()
@@ -17,23 +17,23 @@ good_names <- function(sheet, meta = FALSE) {
     str_replace_all("--", "-") %>%
     str_replace_all("\\..", ".")
 
-  #if(checker == FALSE){
+  if(checker == FALSE){
+    working_names$. <- working_names$. %>%
+      str_split("-")%>%
+      map( ~ .x %>% str_trunc(3, ellipsis = "") %>% .[. != ""] %>% str_c(collapse = "."))
+
+    good_names <- c("Date", c(working_names[-c(1),]%>%unlist()))%>%
+      as.vector()
+  }
+
+  if(checker == TRUE){
   working_names$. <- working_names$. %>%
     str_split("-")%>%
-    map( ~ .x %>% str_trunc(3, ellipsis = "") %>% .[. != ""] %>% str_c(collapse = "."))
+    map( ~ .x %>% str_trunc(10, ellipsis = "") %>% .[. != ""] %>% str_c(collapse = "."))
 
-  good_names <- c("Date", c(working_names[-c(1),]%>%unlist()))%>%
+  good_names <- c("Variable", c(working_names[-c(1),]%>%unlist()))%>%
     as.vector()
-  #}
-
-  #if(checker == TRUE){
-    # working_names$. <- working_names$. %>%
-    #   str_split("-")%>%
-    #   map( ~ .x %>% str_trunc(10, ellipsis = "") %>% .[. != ""] %>% str_c(collapse = "."))
-    #
-    # good_names <- c("Variable", c(working_names[-c(1),]%>%unlist()))%>%
-    #   as.vector()
-  #}
+  }
 
 
   colnames(sheet) <- good_names
